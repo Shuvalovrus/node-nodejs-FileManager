9cf1334ce687ba2a3f314509e4e332523cadbde8
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-06-11 15:47:00 +0300

    feat: add get name func

---
+++ src/cli/getUserName.js
@@ -0,0 +1,14 @@
+export const getUserName = () => {
+
+    let result = 'Stranger';
+
+    process.argv.forEach((item) => {
+
+        const name = item.split('=').pop();
+
+        if (item.startsWith('--username')) result = name === '' ? result : name;
+
+    })
+
+    return result;
+}
