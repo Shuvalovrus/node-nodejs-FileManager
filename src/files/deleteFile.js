f0038b8101964803c98c163c34357c4ffda81ded
Konstantin Shuvalov <shuvalov.2021@list.ru>
2022-06-12 11:20:18 +0300

    fix: change behavior with absolute path for mv case

--- src/files/deleteFile.js
+++ src/files/deleteFile.js
@@ -1,6 +1,8 @@
 import { unlink } from "fs/promises";
+import { isAbsolute, join } from "path";
 
 export const deleteFile = async (path) => {
+    if (!isAbsolute(path)) path = join(process.cwd(), path);
 
     try {
         await unlink(path)
